f6a6e142523322efc6af5d00a342d2c6323e657c
Loubna <loubna.alfidi@gmail.com>
2024-02-09 03:06:11 +0100

    Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+$(document).ready(function() {
+    let checkedAmenities = {}; // Object to store checked amenities
+
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function() {
+        let amenityID = $(this).data('id'); // Get Amenity ID from data attribute
+        let amenityName = $(this).data('name'); // Get Amenity Name from data attribute
+
+        // If the checkbox is checked, store the Amenity Name in the object
+        if ($(this).is(':checked')) {
+            checkedAmenities[amenityID] = amenityName;
+        } else {
+            // If the checkbox is unchecked, remove the Amenity Name from the object
+            delete checkedAmenities[amenityID];
+        }
+
+        // Create a string of checked amenity names
+        let checkedNames = Object.values(checkedAmenities);
+
+        // Update the h4 tag inside the div Amenities with the list of Amenities checked
+        let amenitiesText = checkedNames.join(', '); // Join the checked names with a comma
+        $('.amenities h4').text(amenitiesText);
+    });
+});
+
